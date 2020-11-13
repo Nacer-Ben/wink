f36bdb782aa05ba3fa52a57c59e6fcd1fc7ab380
Nacer-Ben <nacer-eddine.benhammouda@devinci.fr>
2020-11-13 16:18:43 +0100

    Update InstallCommand.php

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -3,6 +3,9 @@
 namespace Wink\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 use Wink\WinkAuthor;
 
 class InstallCommand extends Command
